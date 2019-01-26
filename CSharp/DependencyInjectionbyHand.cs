d2603bb13d973b74e691a7a4a5df4a251ede2fc6
RajasekharwhY <yakkali.rajasekhar@gmail.com>
2019-01-25 22:16:50 -0500

    Dependency Injection by Hand

---
+++ CSharp/DependencyInjectionbyHand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    class DependencyInjectionbyHand
+    {
+        
+        
+    }
+
+    class soldier
+    {
+        private readonly IWeapon weapon;
+        public soldier(IWeapon Soldierweapon)
+        {
+            weapon = Soldierweapon;
+        }
+
+        public void Attack()
+        {
+            weapon.Hit();
+        }
+    }
+
+    interface IWeapon
+    {
+        void Hit();
+    }
+
+    class sword : IWeapon
+    {
+        public void Hit()
+        {
+            Console.WriteLine("Sword - Cut Into two halfs");
+        }
+    }
+
+    class Chakra : IWeapon
+    {
+        public void Hit()
+        {
+            Console.WriteLine("Chakra - Cut Into two halfs");
+        }
+    }
+}
---
+++ CSharp/ProgramforDependencyInjectionbyHand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    class ProgramforDependencyInjectionbyHand
+    {
+
+        static void Main(string[] args)
+        {
+            soldier s1 = new soldier(new sword()); // Instanciating 
+            soldier s2 = new soldier(new Chakra());
+
+            // This is calleed dependency injection by hand because  
+            // each time you want to create soldier object you need supply
+            // or create a implementation of IWeapon (chakra, sword) and 
+            // pass it to the soldier constructor 
+
+            s1.Attack();
+            s2.Attack();
+
+        }
+    }
+}
