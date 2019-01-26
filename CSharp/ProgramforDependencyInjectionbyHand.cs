69489f4dc2e5af6548ec90af1d88486df3d28402
RajasekharwhY <yakkali.rajasekhar@gmail.com>
2019-01-25 22:22:02 -0500

    Update ProgramforDependencyInjectionbyHand.cs

--- CSharp/ProgramforDependencyInjectionbyHand.cs
+++ CSharp/ProgramforDependencyInjectionbyHand.cs
@@ -18,6 +18,12 @@ namespace code
             // each time you want to create soldier object you need supply
             // or create a implementation of IWeapon (chakra, sword) and 
             // pass it to the soldier constructor 
+            
+            // for smaller projects this creation of objects and pass it to the 
+            // constructor is easy, but for compplex projects and things like
+            // caching, logging, tracing, auditing etc.. it is difficult to 
+            // add for each objects, in these cases containers like Ninject will be
+            // a great help.
 
             s1.Attack();
             s2.Attack();
