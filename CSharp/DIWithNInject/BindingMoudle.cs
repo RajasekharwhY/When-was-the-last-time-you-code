52eed9af8ead84ddb0cb7f50eff5b1025207183b
RajasekharwhY <yakkali.rajasekhar@gmail.com>
2019-01-29 20:59:13 -0500

    Use DI to inject sword
    install Ninject package.

---
+++ CSharp/DIWithNInject/BindingMoudle.cs
@@ -0,0 +1,18 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.DIWithNInject
+{
+    class BindingMoudle : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IWeaponNinect>().To<swordNj>().Named("sword");
+            //Bind<IWeaponNinect>().To<chakraNj>().Named("Chakra");
+        }
+    }
+}
---
+++ CSharp/DIWithNInject/DIwithNinject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace code
+{
+    class DIwithNinject
+    {
+    }
+    
+    class soldier1
+    {
+        readonly IWeaponNinect weaponNj;
+
+       public soldier1(IWeaponNinect weapon)
+        {
+            weaponNj = weapon;
+        }
+
+        public void attackNj()
+        {
+            weaponNj.HitNineject();
+        }
+    }
+
+    interface IWeaponNinect
+    {
+        void HitNineject();
+    }
+
+    class swordNj : IWeaponNinect
+    {
+        public void HitNineject()
+        {
+            Console.WriteLine("Print sword HIT NIninect");
+        }
+    }
+
+    class chakraNj : IWeaponNinect
+    {
+        public void HitNineject()
+        {
+            Console.WriteLine("Print chakra ninect");
+        }
+    }
+}
